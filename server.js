82978323ea3b63adaf376a58703d556048677651
YJohn <yfekadu2016@gmail.com>
2017-08-14 12:00:22 +0100

    create a footer handlebars and created a resource folder and added product-list on it

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const app = express();
 const exphbs = require('express-handlebars');
 const fs = require("fs");
 // const readPosts = require('./helpers/readPosts');
-
+fs.readFile('resources/product-list.txt', function (err, data) {
+    if (err)
+        throw err;
+    if (data)
+        console.log(data.toString('utf8'));
+});
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
