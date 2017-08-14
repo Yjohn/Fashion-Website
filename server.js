caf929647f21c6b430841c5d3a6613ce2dcd9d72
YJohn <yfekadu2016@gmail.com>
2017-08-14 12:04:41 +0100

    create file system on server.js

--- server.js
+++ server.js
@@ -3,12 +3,7 @@ const app = express();
 const exphbs = require('express-handlebars');
 const fs = require("fs");
 // const readPosts = require('./helpers/readPosts');
-fs.readFile('resources/product-list.txt', function (err, data) {
-    if (err)
-        throw err;
-    if (data)
-        console.log(data.toString('utf8'));
-});
+fs.readFile("test.txt", 'utf8', ((err,fileContents) => console.log(fileContents)));
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
